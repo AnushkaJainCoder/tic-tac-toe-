ad7b4db83a6db7cb90215a414801a95301ec82f4
Anushka Jain <102728955+Anushkajain22@users.noreply.github.com>
2024-11-01 20:33:39 +0530

    Square.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Square from './Square';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value = {1}/>
+        {/* <button className='square'>
+          1
+        </button> */}
+         <Square value = {2}/>
+         <Square value = {3}/>
+        
+      </div>
+      <div className="board-row">
+        <Square value = {4}/>
+        {/* <button className='square'>
+          1
+        </button> */}
+         <Square value = {5}/>
+         <Square value = {6}/>
+        
+      </div>
+      <div className="board-row">
+        <Square value = {7}/>
+        {/* <button className='square'>
+          1
+        </button> */}
+         <Square value = {8}/>
+         <Square value = {9}/>
+        
+      </div>
+    </>
+    
+    
+    
   );
 }
 
---
+++ src/Square.js
@@ -0,0 +1,10 @@
+export default function Square({value}){
+    return (
+        <>
+            <button className="square">
+                {value}
+            </button>
+        
+        </>
+    )
+}
